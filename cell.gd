a926fef861349f2b4a8862d4c07323c1a9cefc54
Aru Poleo Vargas <arupoleo@gmail.com>
2024-09-24 09:42:03 -0400

    added params to the cells and a helper function in grid

--- cell.gd
+++ cell.gd
@@ -1,5 +1,11 @@
 class_name Cell extends Node2D
 
+var strength: float
+var instance_name: String
+
+func _init(instance_name, strength = 1):
+	self.instance_name = instance_name
+	self.strength = strength
 
 
 # Called when the node enters the scene tree for the first time.
